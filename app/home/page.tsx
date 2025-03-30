fdfef31a77790c8bd7ff9c535b465a7ebbe96dcf
Vansh Deo <vanshdeo5.mathsg@gmail.com>
2025-03-30 05:31:08 +0530

    Hime Page sorted

--- app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import SwipeCard from "@/components/swipeCard";
-
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-50">
-      <SwipeCard />
-    </main>
-  );
-}
